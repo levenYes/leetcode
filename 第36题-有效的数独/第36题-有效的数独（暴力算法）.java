d06d4f96b27a82f9cd353b739f9a9eb31dfe9c50
liwenye <477716556@qq.com>
2021-06-30 18:03:55 +0800

    Create 第36题-有效的数独（暴力算法）.java

---
+++ 第36题-有效的数独/第36题-有效的数独（暴力算法）.java
@@ -0,0 +1,75 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        boolean isValidSudoku = true;
+        if(!isValidColumn(board)) {
+          isValidSudoku = false;
+        }
+        if(!isValidColumn(board)) {
+          isValidSudoku = false;
+        }
+        if(!isValidNines(board)) {
+          isValidSudoku = false;
+        }
+
+        return isValidSudoku;
+    }
+
+    private boolean isValidColumn(char[][] board) {
+      for(int i=0; i < 9; i++) {
+        Set charSet = new HashSet<String>();
+        for(int j=0; j < 9;j++) {
+          if(board[i][j] != '.') {
+            if(!charSet.add(board[i][j])){
+              return false;
+            }
+          }
+        }
+      }
+      return true;
+    }
+
+    private boolean isValidRow(char[][] board) {
+      for(int i=0; i < 9; i++) {
+        Set charSet = new HashSet<String>();
+        for(int j=0; j < 9;j++) {
+          if(board[j][i] != '.') {
+            if(!charSet.add(board[i][j])){
+              return false;
+            }
+          }
+        }
+      }
+      return true;
+    }
+
+    private boolean isValidNines(char[][] board) {
+      int x = 0;
+      int y = 0;
+      boolean isValidNine = true;
+      while(isValidNine && x < 7){
+        y = 0;
+        while(y < 7) {
+          isValidNine = isValidNine(board, x, y );
+          y ++;
+        }
+        x = x +3;
+      }
+
+      return isValidNine;
+    }
+
+    private boolean isValidNine(char[][] board, int x, int y) {
+      Set charSet = new HashSet<String>();
+      for(int i = x; i< i+3; i ++) {
+        for(int j = y; j< j+3; j++) {
+          if(board[j][i] != '.') {
+            if(!charSet.add(board[i][j])){
+              return false;
+            }
+          }
+        }
+      }
+
+      return true;
+    }
+}
